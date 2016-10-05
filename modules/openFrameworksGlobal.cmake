6de787dc17d7088a2845f8c61722ff7396b2e2c5
Peter Kolski <peter@bildpeter.de>
2016-10-05 03:18:07 +0200

    setting an output dir

--- modules/openFrameworksGlobal.cmake
+++ modules/openFrameworksGlobal.cmake
@@ -7,5 +7,9 @@ if( APPLE )
     include( ${OF_DIRECTORY}/CMake/modules/openFrameworksApple.cmake )
 endif()
 
+# ===================== Library installation path ========
+#install( FILES ${APP_NAME} LIBRARY DESTINATION ${OF_DIRECTORY}/CMake/libs )
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${OF_DIRECTORY}/CMake/libs )
 # ===================== App Properties ===================
 set_target_properties(${APP_NAME} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/bin)
+
